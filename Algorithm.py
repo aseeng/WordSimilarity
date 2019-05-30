df5dfa36da751339f8710db7d2a6b643b735175d
aseeng <alessandromarrazzo9@gmail.com>
2019-05-30 16:04:45 +0200

    added common ancestor

--- Algorithm.py
+++ Algorithm.py
@@ -1,32 +1,72 @@
+from builtins import print
+
 from nltk.corpus import wordnet as wn
 import math
 import numpy as np
 
+def findAllHypernym(syn,hypernym):
+
+    s = syn.hypernyms()
+    ent = wn.synset('entity.n.01')
+    if len(s) == 0:
+        hypernym.append(syn)
+        return
+
+    if s[0] == ent:
+
+        hypernym.append(s[0])
+        return
+
+    hypernym.append(s[0])
+    findAllHypernym(s[0], hypernym)
 
 def similarity(syn1,syn2):
 
-    commonAncestor = syn1.lowest_common_hypernyms(syn2)
-    depthLCS = commonAncestor[0].max_depth()
+    print(syn1)
+    print(syn2)
+
+    hypernym1 = []
+    hypernym2 = []
+
+    findAllHypernym(syn1, hypernym1)
+    findAllHypernym(syn2, hypernym2)
+
+    commonAncestor = ""
+    depthLCS = 0
+
+    for i in hypernym1:
+        if i in hypernym2:
+            commonAncestor = i
+            break
+
+    if not commonAncestor is "":
+        depthLCS = commonAncestor.max_depth()
+
+    else:
+        depthLCS = -1
 
     depth1 = syn1.max_depth()
     depth2 = syn2.max_depth()
 
-    length = depth1 + depth2 - 2 * depthLCS
-    DEPTH_MAX = 18
     value = []
+    DEPTH_MAX = 18
 
-    #Wu Palmer
-    value.append(2 * depthLCS / (depth1 + depth2))
+    if not depthLCS == -1:
+        length = depth1 + depth2 - 2 * depthLCS
 
-    #Shortest Path
-    value.append(2 * DEPTH_MAX - length)
+        #Wu Palmer
+        value.append(2 * depthLCS / (depth1 + depth2))
 
-    #LeakcockChodorow
-    value.append(math.log2((length + 1) / 2 * (DEPTH_MAX + 1)))
+        #Shortest Path
+        value.append(2 * DEPTH_MAX - length)
+
+        #LeakcockChodorow
+        value.append(math.log2((length + 1) / 2 * (DEPTH_MAX + 1)))
 
     return value
 
 def findSimilarity(lemma1 , lemma2):
+
 
     synset1 = wn.synsets(lemma1,pos="n")
     synset2 = wn.synsets(lemma2,pos="n")
